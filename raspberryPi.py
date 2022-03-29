30988746ebc908a8d57f5bdd06063c26ab359845
Even <even@EvendeMacBook-Pro.local>
2022-03-29 13:26:47 +0100

    t

--- raspberryPi.py
+++ raspberryPi.py
@@ -15,8 +15,9 @@ from PIL import Image, ImageTk
 
 factory = PiGPIOFactory()
 # factory = PiGPIOFactory(host='192.168.0.58') # 填写树莓派的IP地址
+# factory = PiGPIOFactory(host='10.126.159.234')
 btn_rotor = Button(14, pin_factory=factory)
-rotor = RotaryEncoder(15, 18, wrap=True, pin_factory=factory)
+rotor = RotaryEncoder(15, 18, max_steps=20, threshold_steps=(3, 4), wrap=True, pin_factory=factory)
 
 # btn_red = Button(24, pin_factory=factory)
 # btn_blue = Button(23, pin_factory=factory)
@@ -33,24 +34,30 @@ test_feedback = pages_dir + '/test_feedback.png'
 
 news_pages = []
 page_num = 0
+page_map= {}
 red_chosen = []
 blue_chosen = []
 
 def load_pages():
+    for i in range(0, 6):
+        page_map[3 * i] = i
+        page_map[3 * i + 1] = i
+        page_map[3 * i + 2] = i
     for root, dirs, files in os.walk(pages_dir):
         for name in files:
             if '.png' in name:
                 news_pages.append(os.path.join(root, name))
 
 
-def route_pages(page):
-    return page
+def route_pages():
+    news_pic.image = news_pages[page_num]
 
 def next_news():
     global page_num
     page_num += 1
-    news_pic.image = news_pages[page_num]
-    return news_pages[page_num]
+    if
+    # news_pic.image = news_pages[page_num]
+    # return news_pages[page_num]
 
 def last_news():
     global page_num
@@ -85,13 +92,14 @@ def rotor_rotated():
 load_pages()
 
 print(news_pages)
+print(page_map)
 app = App(title="guizero", width=800, height=480)
 
 # intro = Text(app, text="Welcome to InfoClinic", size=40, font="Monaco", color="lavender")
 # text_box = TextBox(app)
 # ok = PushButton(app, command=text_update, text="mes")
 news_pic = Picture(app, image=test_welcome, width=800, height=450)
-news_pic.when_left_button_pressed = next_news
+news_pic.when_left_button_pressed = route_pages
 news_pic.when_right_button_pressed = last_news
 btn_red = PushButton(app, command=red_touched, visible=False)
 btn_blue = PushButton(app, command=blue_touched, visible=False)
