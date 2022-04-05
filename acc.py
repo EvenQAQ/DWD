4d376a53565e09d184d70c45201c9701e84d2264
Even <even@EvendeMacBook-Pro.local>
2022-04-05 16:56:59 +0100

    mpu

--- acc.py
+++ acc.py
@@ -1,85 +1,27 @@
-#coding=utf-8
-
-#coding:utf-8
-
-import smbus
-import math
+import FaBo9Axis_MPU9250
 import time
-import keyboard
+import sys
 
-# 电源控制寄存器地址
-power_regist = 0x6b
+mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 
-# I2C模块初始化
-bus = smbus.SMBus(1)
-# 外接I2C设备的地址
-address = 0x68
+try:
+    while True:
+        accel = mpu9250.readAccel()
+        print(" ax = " , ( accel['x'] ))
+        print(" ay = " , ( accel['y'] ))
+        print(" az = " , ( accel['z'] ))
 
-# 封装一些读取数据的功能函数
+        gyro = mpu9250.readGyro()
+        print(" gx = " , ( gyro['x'] ))
+        print(" gy = " , ( gyro['y'] ))
+        print(" gz = " , ( gyro['z'] ))
 
-# 读取一个字长度的数据(16位)
-def readWord(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
+        mag = mpu9250.readMagnet()
+        print(" mx = " , ( mag['x'] ))
+        print(" my = " , ( mag['y'] ))
+        print(" mz = " , ( mag['z'] ))
 
-# 将读取到的数据转换为原码 (有符号数本身是采用补码方式存储的)
-def readWordReal(adr):
-    val = readWord(adr)
-    x = 0xffff
-    # 首位为1 表示是负数
-    if (val >= 0x8000):
-        # 求原码
-        return -((x - val)+1)
-    else:
-        return val
+        time.sleep(0.1)
 
-# 已知加速度求角度值
-def dist(a, b):
-    return math.sqrt((a*a)+(b*b))
-
-def getRotationX(x, y, z):
-    radians = math.atan2(y, dist(x,z))
-    return math.degrees(radians)
-
-def getRotationY(x, y, z):
-    radians = math.atan2(x, dist(y,z))
-    return math.degrees(radians)
-
-
-# 设置电源模式
-bus.write_byte_data(address, power_regist, 0)
-
-
-while True:
-    if keyboard.is_pressed("s"):
-        print("-------------")
-    time.sleep(0.5)
-    # print("螺旋仪数据-----------")
-    # gyroX = readWordReal(0x43)
-    # gyroY = readWordReal(0x45)
-    # gyroZ = readWordReal(0x47)
-
-    # print("X轴陀螺仪原始数据：", gyroX, "X轴每秒旋转度数：", gyroX/131)
-    # print("Y轴陀螺仪原始数据：", gyroY, "Y轴每秒旋转度数：", gyroY/131)
-    # print("Z轴陀螺仪原始数据：", gyroZ, "Z轴每秒旋转度数：", gyroZ/131)
-
-    # print("加速度数据----------")
-    accelX = readWordReal(0x3b)
-    accelY = readWordReal(0x3d)
-    accelZ = readWordReal(0x3f)
-
-    # print("X轴加速度原始数据：", accelX, "X轴加速度：", accelX/16384)
-    # print("Y轴加速度原始数据：", accelY, "Y轴加速度：", accelY/16384)
-    # print("Z轴加速度原始数据：", accelZ, "Z轴加速度：", accelZ/16384)
-
-    # print("摄氏温度数据--------")
-    # temp = readWordReal(0x41)
-    # print("温度原始数据：", temp, "摄氏度：", temp/340 + 36.53)
-
-    # print("旋转家角度数据-------")
-    print(getRotationX(accelX/16384, accelY/16384, accelZ/16384), getRotationY(accelX/16384, accelY/16384, accelZ/16384))
-
-
-
+except KeyboardInterrupt:
+    sys.exit()
