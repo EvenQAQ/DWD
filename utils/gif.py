c60d13893380d31d3c85a171adb0a96149a811c7
Even <even@EvendeMacBook-Pro.local>
2022-04-08 02:25:26 +0100

    transfer

--- utils/gif.py
+++ utils/gif.py
@@ -1,7 +1,7 @@
 import moviepy.editor as mpy
 
 #视频文件的本地路径
-content = mpy.VideoFileClip("/Users/even/Dev/DWD/loading.mp4")
+content = mpy.VideoFileClip("/Users/even/Dev/DWD/assets/loading.mp4")
 # 剪辑0分0秒到0分4秒的片段。resize为修改清晰度
 c1 = content.subclip((0,0),(0,11)).resize((800,450))
 # 将片段保存为gif图到python的默认路径
